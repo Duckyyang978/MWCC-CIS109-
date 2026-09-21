0e18bc0f0a63ef1dca5f928691de98b60d67d820
Duckyyang978 <isaacyang978@gmail.com>
2026-09-20 21:01:32 -0400

    Add files via upload

---
+++ Seceret Mission.py
@@ -0,0 +1,25 @@
+print("Isaac")
+print("Age: 19")
+print("mission code:Isaac-red-19")
+
+print("\n training: 4")
+print("training percentage: 26%")
+
+
+print("\ngadgets: 4")
+print("gadgets density: 1 per training year ")
+
+print("\n mission time: 20 minutes") 
+print("mission time left: 20 minutes") 
+print ("mission time in seconds:1200 seconds")
+
+Age = 19
+if Age >= 18:
+    is_adult = True
+else:
+    is_adult = False
+
+gadgets = 4 
+if gadgets >= 5: 
+        has_many_gadgets = True
+else: has_many_gadgets = False
